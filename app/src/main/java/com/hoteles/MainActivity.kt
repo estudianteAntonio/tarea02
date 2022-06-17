5498709d423b62fa86f97ea00fbdf84b73cb9b9c
JAntonioFloresS <jflores40075@ufide.ac.cr>
2022-06-17 14:08:40 -0600

    Primer Intento

---
+++ app/src/main/java/com/hoteles/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.hoteles
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.hoteles.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        FirebaseApp.initializeApp(this)
+        auth = Firebase.auth
+
+        binding.btRegister.setOnClickListener { realizaRegistro() }
+        binding.btLogin.setOnClickListener { realizarLogin() }
+
+    }
+    private fun realizaRegistro() {
+        val email = binding.etEmail.text.toString()
+        val pass = binding.etPassword.text.toString()
+
+        //Se usa la función para crear un usuario por medio de correo y contraseña
+        auth.createUserWithEmailAndPassword(email,pass)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    actualice(user)
+                } else {
+                    Toast.makeText(baseContext,
+                        getString(R.string.msg_fallo_registro),
+                        Toast.LENGTH_SHORT).show()
+                    actualice(null)
+                }
+            }
+    }
+
+
+    private fun realizarLogin() {
+        val email = binding.etEmail.text.toString()
+        val pass = binding.etPassword.text.toString()
+
+        auth.signInWithEmailAndPassword(email,pass)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    actualice(user)
+                } else {
+                    Toast.makeText(baseContext,
+                        getString(R.string.msg_fallo_login),
+                        Toast.LENGTH_SHORT).show()
+                    actualice(null)
+                }
+            }
+    }
+    private fun actualice(user: FirebaseUser?) {
+        if (user!=null) {
+            val intent = Intent(this,Principal::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
